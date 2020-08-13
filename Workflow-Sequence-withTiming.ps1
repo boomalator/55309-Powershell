d975ef5c6247d6dd5cfe365dba9c4257b7b20ba8
BP <bph@byronetta.com>
2020-08-13 16:11:28 -0400

    workflow

---
+++ Workflow-Sequence-withTiming.ps1
@@ -0,0 +1,21 @@
+﻿Workflow Test-Workflow {
+    "This will run first"
+
+    parallel {
+        "$((get-date).tostring('mm:ss.fff')) Command 1"
+        Sequence { 
+            Start-Sleep -Seconds 3         
+            "$((get-date).tostring('mm:ss.fff')) Command 3"
+            }
+        "$((get-date).tostring('mm:ss.fff')) Command 2"
+    
+        sequence {
+            "$((get-date).tostring('mm:ss.fff')) Command A"
+            Start-Sleep -Seconds 2
+            "$((get-date).tostring('mm:ss.fff')) Command B"
+        }
+    }
+}
+
+
+Test-Workflow
