db5d6572e53bab9f6d694bb2afa3c417da46235b
BP (Home) <bph@byronetta.com>
2020-10-19 00:09:08 -0600

    Add GK Specifics. Clean up Slides.

---
+++ date-param-ex.ps1
@@ -0,0 +1,13 @@
+﻿
+$diff = New-TimeSpan -Start 'September 19,2020' -End 'December 6, 1944'
+Write-Output "Time difference is: $($diff.TotalDays/365.25) years"
+
+
+
+
+$dtoday = [datetime] 'September 19,2020'
+$dpast = [datetime] 'December 6, 1944'
+$diff = New-TimeSpan -Start $dpast -End $dtoday
+Write-Output "Time difference is: $($diff.TotalDays/365.25) years"
+
+
