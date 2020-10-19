c702c0fe82093c1e9d96121f22e63bf48bb031b2
BP (Home) <bph@byronetta.com>
2020-10-19 15:09:06 -0600

    Reorganized

---
+++ Day 1 - Language, Scripts, Functions/Get-DateDiff.ps1
@@ -0,0 +1,22 @@
+﻿
+function say-Hello {
+    Write-Host "Hello World!" -ForegroundColor White
+}
+function get-DateDifference { 
+    [CmdletBinding()]
+    param (
+        [datetime] $dpast ,
+        [datetime] $dtoday = (get-date)
+    )
+
+    #$dtoday = [datetime] 'October 19,2020'
+    #$dpast = [datetime] 'December 6, 1944'
+
+    $diff = New-TimeSpan -Start $dpast -End $dtoday
+    Write-Output "Time difference is: $($diff.TotalDays/365.25) years"
+}
+
+get-DateDifference -dpast 'September 11, 1911'
+get-DateDifference -dpast 'September 11, 1939'
+get-DateDifference -dpast 'September 11, 1973'
+
---
+++ Day 1 - Language, Scripts, Functions/date-param-ex.ps1
@@ -0,0 +1,11 @@
+﻿
+# $diff = New-TimeSpan -End 'October 19,2020' -Start 'November 27, 1987'
+# Write-Output "Time difference is: $($diff.TotalDays/365.25) years"
+
+$dtoday = [datetime] 'October 19,2020'
+$dpast = [datetime] 'December 6, 1944'
+
+$diff = New-TimeSpan -Start $dpast -End $dtoday
+Write-Output "Time difference is: $($diff.TotalDays/365.25) years"
+
+
