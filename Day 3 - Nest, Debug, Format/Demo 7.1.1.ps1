7f1dc0df42ffecac1618a82d31371df5fcd75865
BP <bph@byronetta.com>
2020-10-21 10:10:06 -0400

    Consolidate and re-org  demos

---
+++ Day 3 - Nest, Debug, Format/Demo 7.1.1.ps1
@@ -0,0 +1,56 @@
+﻿<#
+Part 1: This is the buggy command
+What will this do? What is the EXPECTATION?
+What SHOULD happen? What is the INTENDED OUTPUT?
+#>
+Get-CimInstance -class Win32_LogicalDisk `
+                -filter "drivetype='fixed'" |
+Select -Property DeviceID,Size |
+Sort -Property FreeSpace 
+
+
+
+<#
+Part 2: The next step is to start with just
+a single command instead of a whole pipeline.
+This helps narrow down the source of the error.
+#>
+Get-CimInstance -class Win32_LogicalDisk `
+                -filter "drivetype='fixed'"
+
+
+<#
+Part 3: Now start removing parameters until
+the error is gone
+#>
+Get-CimInstance -class Win32_LogicalDisk
+
+
+<#
+Part 4: Once it works, pipe the output to GM
+and verify expectations - in this case,
+DriveType is a number, not a string.
+#>
+Get-CimInstance -class Win32_LogicalDisk | Get-Member
+
+
+<#
+Part 5: Then start reconstructing the original command
+A problem here is that we're sorting by a property that
+doesn't exist at that point in the pipeline
+#>
+Get-CimInstance -class Win32_LogicalDisk |
+Select -Property DeviceID,Size |
+Sort -Property FreeSpace 
+
+
+<#
+Part 6: We can prove by backing off one command
+and using GM
+#>
+Get-CimInstance -class Win32_LogicalDisk |
+Select -Property DeviceID,Size |
+Get-Member
+
+
+# No proceed to Demo 7.1.1B.
---
+++ Demos/Something.ps1
@@ -0,0 +1,3 @@
+$x = "blah"
+Write-Host "Hello"
+$x
