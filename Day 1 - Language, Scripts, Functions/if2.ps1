142bddc8c9109e142143720d332a73680431db52
BP Hynes (SMSec) <bph@byronetta.com>
2021-06-15 09:31:09 -0600

    Create if2.ps1
    Adding IF example

---
+++ Day 1 - Language, Scripts, Functions/if2.ps1
@@ -0,0 +1,18 @@
+$x = 987458647
+
+if ($x -eq 1) 
+{ 
+    Write-Host "We're number 1!" 
+}
+else 
+{ 
+
+    if ($x -eq 2)
+    {
+        Write-Host "So close, and yet so far."
+    }
+    else 
+    {
+        Write-Host "Maybe next year :( " 
+    }
+}  
